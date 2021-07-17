d3cdc9e432b1271df7b81d0deb3c7b6e256894b4
groundlevelgeek <groundlevelgeek@gmail.com>
2021-07-17 20:57:25 +0530

    Enable bubble sort for array

---
+++ src/main/java/BubbleSorting.java
@@ -0,0 +1,37 @@
+public class BubbleSorting {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[]{20, 35, -15, 7, 55, 1, -22};
+
+        System.out.println("\n\n\nGiven Unsorted Array: -------\n");
+        for (int value : array) {
+            System.out.print(value + " , ");
+        }
+
+        int bubbleIndex = array.length - 1;
+
+        while (bubbleIndex > 0) {
+            var noSwap = true;
+            for (int index = 0; index < bubbleIndex; index++) {
+                if (array[index] > array[index + 1]) {
+                    var holder = array[index];
+                    array[index] = array[index + 1];
+                    array[index + 1] = holder;
+                    noSwap = false;
+                }
+            }
+            bubbleIndex -= 1;
+            if (noSwap) {
+                return;
+            }
+        }
+
+        System.out.println("\n\nSorted Array using Bubble Sort: ----------\n");
+        for (int value : array) {
+            System.out.print(value + " , ");
+        }
+        System.out.println("\n\n");
+    }
+
+}
